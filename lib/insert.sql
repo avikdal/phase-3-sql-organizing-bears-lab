74b96ff5031372ed338a24f98d1a5719c6258a50
amanda vikdal <vikdal.amanda@gmail.com>
2023-08-30 15:49:03 -0400

    commit tree 649a9afd6af7b29209d4d7868d0968ae0557e7ad

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr.Chocolate', 3, M, 'brown','happy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 4, M, 'blue','mellow', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 15, F, 'black','happy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 5, F, 'white','serious', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 16, F, 'purple','happy', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 2, F, 'green','grumpy', 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 8, F, 'pink','mischevious', 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 10, F, 'black','happy', 0);
+
+
+
+
+
+
+
